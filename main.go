ab6b6f99b12ea7e3ee64ddcf333a149d275baecb
Stewart Platt <shteou@gmail.com>
2020-04-27 15:56:43 +0100

    ooo

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("title: %s\n", title)
 	}
 
-	fmt.Fprintf(w, "Hello from:  "+title+"\nWoop!!!!!!!!!")
+	fmt.Fprintf(w, "Hello from:  "+title+"\nWoop!!!!!!!!!!!")
 }
 
 func main() {
